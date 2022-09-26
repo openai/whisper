312ca71f537f35d66936b523a48a053f54a36ac4
Michael Goin <michael@neuralmagic.com>
2022-09-26 12:53:59 -0400

    Use PyTorch as logits transpose for ONNX support

--- whisper/model.py
+++ whisper/model.py
@@ -189,7 +189,7 @@ class TextDecoder(nn.Module):
             x = block(x, xa, mask=self.mask, kv_cache=kv_cache)
 
         x = self.ln(x)
-        logits = (x @ self.token_embedding.weight.to(x.dtype).T).float()
+        logits = (x @ torch.transpose(self.token_embedding.weight.to(x.dtype), 0, 1)).float()
 
         return logits
 
