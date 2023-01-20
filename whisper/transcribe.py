12e1089462a2ea92e9ade6145e7be5b6883676ff
Jong Wook Kim <jongwook@openai.com>
2023-01-20 00:54:05 -0800

    use stdout for printing transcription progress (#867)

--- whisper/transcribe.py
+++ whisper/transcribe.py
@@ -169,7 +169,8 @@ def transcribe(
             line = f"[{format_timestamp(start)} --> {format_timestamp(end)}] {text}\n"
             # compared to just `print(line)`, this replaces any character not representable using
             # the system default encoding with an '?', avoiding UnicodeEncodeError.
-            sys.stderr.buffer.write(line.encode(sys.getdefaultencoding(), errors="replace"))
+            sys.stdout.buffer.write(line.encode(sys.getdefaultencoding(), errors="replace"))
+            sys.stdout.flush()
 
     # show the progress bar when verbose is False (otherwise the transcribed text will be printed)
     num_frames = mel.shape[-1]
