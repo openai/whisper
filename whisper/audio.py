06dc24a5ad0895bc28a6e5f39f776045b3705784
johnnynunez <johnnynuca14@gmail.com>
2023-03-31 00:26:18 +0200

    fix

--- whisper/audio.py
+++ whisper/audio.py
@@ -24,8 +24,6 @@ FRAMES_PER_SECOND = exact_div(SAMPLE_RATE, HOP_LENGTH)  # 10ms per audio frame
 TOKENS_PER_SECOND = exact_div(SAMPLE_RATE, N_SAMPLES_PER_TOKEN)  # 20ms per audio token
 
 
-
-
 def load_audio(file: str, sr: int = SAMPLE_RATE):
     """
     Open an audio file and read as mono waveform, resampling as necessary
