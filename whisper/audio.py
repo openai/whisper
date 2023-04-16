fb59e14d0f536092e9ed086e478eb500d1e02ad8
Petter Reinholdtsen <pere@hungry.com>
2023-04-16 15:31:18 +0000

    Drop ffmpeg-python dependency and call ffmpeg directly.
    The last ffmpeg-python module release was in 2019[1], upstream seem to be
    unavailable[2] and the project development seem to have stagnated[3].  As
    the features it provide is trivial to replace using the Python native
    subprocess module, drop the dependency.
    
     [1] <URL: https://github.com/kkroening/ffmpeg-python/tags >
     [2] <URL: https://github.com/kkroening/ffmpeg-python/issues/760 >
     [3] <URL: https://openhub.net/p/ffmpeg-python >

--- whisper/audio.py
+++ whisper/audio.py
@@ -1,8 +1,8 @@
 import os
 from functools import lru_cache
+from subprocess import Popen, PIPE, DEVNULL
 from typing import Optional, Union
 
-import ffmpeg
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -39,19 +39,26 @@ def load_audio(file: str, sr: int = SAMPLE_RATE):
     -------
     A NumPy array containing the audio waveform, in float32 dtype.
     """
+
+    p = None
     try:
-        # This launches a subprocess to decode audio while down-mixing and resampling as necessary.
-        # Requires the ffmpeg CLI and `ffmpeg-python` package to be installed.
-        out, _ = (
-            ffmpeg.input(file, threads=0)
-            .output("-", format="s16le", acodec="pcm_s16le", ac=1, ar=sr)
-            .run(cmd=["ffmpeg", "-nostdin"], capture_stdout=True, capture_stderr=True)
-        )
-    except ffmpeg.Error as e:
-        raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
-
-    return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
-
+        # This launches a subprocess to decode audio while down-mixing
+        # and resampling as necessary.  Requires the ffmpeg CLI in PATH.
+        cmd = ['ffmpeg',
+               '-nostdin',
+               '-threads', '0',
+               '-i', file,
+               '-f', 's16le',
+               '-ac', '1',
+               '-acodec', 'pcm_s16le',
+               '-ar', str(sr),
+               '-']
+        p = Popen(cmd, stdout=PIPE, stderr=DEVNULL)
+        return np.frombuffer(p.stdout.read(),
+                             np.int16).flatten().astype(np.float32) / 32768.0
+    finally:
+        if p:
+            p.terminate()
 
 def pad_or_trim(array, length: int = N_SAMPLES, *, axis: int = -1):
     """
