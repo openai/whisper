376acb29345d9c5d5863b71438180b722de7e504
Alejandro Molina <amolinasalazar@gmail.com>
2023-10-22 17:41:37 +0200

    ADD logic for max_words_count

--- whisper/utils.py
+++ whisper/utils.py
@@ -104,7 +104,9 @@ class SubtitlesWriter(ResultWriter):
         raw_max_line_width: Optional[int] = options["max_line_width"]
         max_line_count: Optional[int] = options["max_line_count"]
         highlight_words: bool = options["highlight_words"]
+        max_words_count: Optional[int] = options["max_words_count"]
         max_line_width = 1000 if raw_max_line_width is None else raw_max_line_width
+        max_words_count = 1000 if max_words_count is None else max_words_count
         preserve_segments = max_line_count is None or raw_max_line_width is None
 
         def iterate_subtitles():
@@ -114,34 +116,41 @@ class SubtitlesWriter(ResultWriter):
             subtitle: list[dict] = []
             last = result["segments"][0]["words"][0]["start"]
             for segment in result["segments"]:
-                for i, original_timing in enumerate(segment["words"]):
-                    timing = original_timing.copy()
-                    long_pause = not preserve_segments and timing["start"] - last > 3.0
-                    has_room = line_len + len(timing["word"]) <= max_line_width
-                    seg_break = i == 0 and len(subtitle) > 0 and preserve_segments
-                    if line_len > 0 and has_room and not long_pause and not seg_break:
-                        # line continuation
-                        line_len += len(timing["word"])
-                    else:
-                        # new line
-                        timing["word"] = timing["word"].strip()
-                        if (
-                            len(subtitle) > 0
-                            and max_line_count is not None
-                            and (long_pause or line_count >= max_line_count)
-                            or seg_break
-                        ):
-                            # subtitle break
-                            yield subtitle
-                            subtitle = []
-                            line_count = 1
-                        elif line_len > 0:
-                            # line break
-                            line_count += 1
-                            timing["word"] = "\n" + timing["word"]
-                        line_len = len(timing["word"].strip())
-                    subtitle.append(timing)
-                    last = timing["start"]
+                chunk_index = 0
+                words_count = max_words_count
+                while chunk_index < len(segment["words"]):
+                    remaining_words = len(segment["words"]) - chunk_index
+                    if max_words_count > len(segment["words"]) - chunk_index:
+                        words_count = remaining_words
+                    for i, original_timing in enumerate(segment["words"][chunk_index:chunk_index + words_count]):
+                        timing = original_timing.copy()
+                        long_pause = not preserve_segments and timing["start"] - last > 3.0
+                        has_room = line_len + len(timing["word"]) <= max_line_width
+                        seg_break = i == 0 and len(subtitle) > 0 and preserve_segments
+                        if line_len > 0 and has_room and not long_pause and not seg_break:
+                            # line continuation
+                            line_len += len(timing["word"])
+                        else:
+                            # new line
+                            timing["word"] = timing["word"].strip()
+                            if (
+                                len(subtitle) > 0
+                                and max_line_count is not None
+                                and (long_pause or line_count >= max_line_count)
+                                or seg_break
+                            ):
+                                # subtitle break
+                                yield subtitle
+                                subtitle = []
+                                line_count = 1
+                            elif line_len > 0:
+                                # line break
+                                line_count += 1
+                                timing["word"] = "\n" + timing["word"]
+                            line_len = len(timing["word"].strip())
+                        subtitle.append(timing)
+                        last = timing["start"]
+                    chunk_index += max_words_count
             if len(subtitle) > 0:
                 yield subtitle
 
