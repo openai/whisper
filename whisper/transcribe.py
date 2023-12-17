e7d46c9ed1df01f551d3b4bfb5f7e5ba310cfffb
Purfview <69023953+Purfview@users.noreply.github.com>
2023-12-17 05:52:12 +0000

    Bugfix: Illogical "Avoid computing higher temperatures on no_speech"
    Bugfix for https://github.com/openai/whisper/pull/1279
    
    It's "silence" when decoding has failed due to `compression_ratio_threshold` too, when further down the code it's not "silence" anymore.
    
    "Silence" should be only when decoding has failed due to `logprob_threshold`.
    
    Like described there:
    https://github.com/openai/whisper/blob/8bc8860694949db53c42ba47ddc23786c2e02a8b/whisper/transcribe.py#L421
    
    And in code there:
    https://github.com/openai/whisper/blob/8bc8860694949db53c42ba47ddc23786c2e02a8b/whisper/transcribe.py#L243-L251

--- whisper/transcribe.py
+++ whisper/transcribe.py
@@ -208,6 +208,7 @@ def transcribe(
             if (
                 no_speech_threshold is not None
                 and decode_result.no_speech_prob > no_speech_threshold
+                and decode_result.avg_logprob < logprob_threshold
             ):
                 needs_fallback = False  # silence
             if not needs_fallback:
