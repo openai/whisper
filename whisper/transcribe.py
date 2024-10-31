ed24c081666c1a7da02075b7028444be3fd882b9
Kent Slaney <kent@slaney.org>
2024-10-31 09:33:47 -0700

    all_tokens setter

--- whisper/transcribe.py
+++ whisper/transcribe.py
@@ -279,7 +279,7 @@ class Transcriber(metaclass=PassthroughPropertyDefaults):
     _initial_finalized: bool = False
     _all_tokens: Optional[list] = None
 
-    @property
+    @PassthroughProperty[Optional[list]](None).property
     def all_tokens(self):
         if self._all_tokens is None:
             self._all_tokens = []
